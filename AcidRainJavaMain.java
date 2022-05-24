2c9c9df7fe06ceaa1f315be9cca5539b84355688
dahyunK <tkdendgud77@naver.com>
2022-05-25 02:19:31 +0900

    Insert game music

--- AcidRainJavaMain.java
+++ AcidRainJavaMain.java
@@ -4,7 +4,9 @@ public class AcidRainJavaMain {
 	public static void main(String[] args) {
 		AcidRainJava acidrainJava = new AcidRainJava();
 		Thread rain = new Thread(acidrainJava.new Rain_Thread());
-		rain.run();
+		Thread rain1 = new Thread(acidrainJava.new PlayWav());
+		rain.start();
+		rain1.start();
 	}
 
 }
